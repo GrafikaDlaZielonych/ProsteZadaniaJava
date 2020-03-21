49554aa77767570ea6b917bf1190864385fcb62b
GrafikaDlaZielonych <62481979+GrafikaDlaZielonych@users.noreply.github.com>
2020-03-21 20:38:37 +0100

    Create Main.java

---
+++ Odc8NajkrotszyZapisOdliczania/Main.java
@@ -0,0 +1,12 @@
+package JavaZadanka.odc8;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int x = 11;
+
+        while( x-- > 0 )
+            System.out.println(x);
+    }
+}
